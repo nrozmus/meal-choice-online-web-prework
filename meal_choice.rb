98b578d894ebddc2310fc2d4112b293952c75900
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-15 02:32:42 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,3 +1,3 @@
-    def greeting(name="Ruby programmer", language="Ruby")
+    def meal_choice(name="meat", language="Ruby")
       puts "Hello, #{name}. We heard you are a great #{language} programmer."
     end
