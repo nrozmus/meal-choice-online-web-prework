ce29c055cbdca484b742b34db54b0d7637ceeceb
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-15 02:33:13 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,3 +1,7 @@
-    def meal_choice(name="meat", language="Ruby")
-      puts "Hello, #{name}. We heard you are a great #{language} programmer."
-    end
+
+def choice(meal_choice="meat")
+    puts "The Meal Choice is #{meal_choice}!"
+end
+def meal_choice(name = "meat")
+  meal_choice = "#{name}"
+end 
